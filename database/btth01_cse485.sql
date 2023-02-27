7dcf1b22e3dc1154dd107e00187557d019764086
Nguyễn Mạnh Hoàng <93708255+asu2812@users.noreply.github.com>
2023-02-27 20:39:29 +0700

    Update btth01_cse485.sql

--- database/btth01_cse485.sql
+++ database/btth01_cse485.sql
@@ -1,59 +1,202 @@
-#Liệt kê các bài viết về các bài hát thuộc thể loại Nhạc trữ tình
-select * from baiviet where baiviet.ma_tloai = "2";
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:4306
+-- Generation Time: Feb 27, 2023 at 10:20 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
-#Liệt kê các bài viết của tác giả “Nhacvietplus”
-select * from baiviet WHERE baiviet.ma_tgia = "1";
-
-#Liệt kê các thể loại nhạc chưa có bài viết cảm nhận nào. 
-select theloai.ten_tloai from theloai,baiviet WHERE theloai.ma_tloai=baiviet.ma_tloai and baiviet.noidung = null;
-
-#Liệt kê các bài viết với các thông tin sau: mã bài viết, tên bài viết, tên bài hát, tên tác giả, tên thể loại, ngày viết.
-SELECT baiviet.ma_bviet,baiviet.tieude,baiviet.ten_bhat,tacgia.ten_tgia,theloai.ten_tloai,baiviet.ngayviet from theloai,tacgia,baiviet 
-WHERE baiviet.ma_tloai = theloai.ma_tloai and baiviet.ma_tgia=tacgia.ma_tgia;
-
-#Tìm thể loại có số bài viết nhiều nhất 
-SELECT theloai.ten_tloai from baiviet,theloai WHERE baiviet.ma_tloai=theloai.ma_tloai 
-group by theloai.ten_tloai 
-having count(theloai.ma_tloai) >= all(
-	SELECT count(baiviet.ma_tloai) from baiviet
-    group by baiviet.ma_tloai
-);
-
-#Liệt kê 2 tác giả có số bài viết nhiều nhất
-SELECT tacgia.ten_tgia FROM tacgia,baiviet where tacgia.ma_tgia = baiviet.ma_tgia
-group by tacgia.ten_tgia
-having count(baiviet.ma_tgia) >= (
-	SELECT top(2)  count(baiviet.ma_tgia)
-    FROM baiviet
-    group by baiviet.ma_tgia
-    DESC
-);
-
-   
-/*liệt kê các bài viết có tự bài hát chứa 1 trong các từ "yêu","thương","anh","em"*/
-SELECT*from baiviet where ten_bhat like '%yêu%'or  ten_bhat like '%thương%' or ten_bhat like '%anh%'or ten_bhat like '%em%';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-/*Liệt kê các bài viết về các bài hát có tiêu đề bài viết hoặc tựa bài hát chứa 1 trong các từ  "yêu","thương","anh","em"*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-select*from baiviet where ten_bhat like '%yêu%'or  ten_bhat like '%thương%' or ten_bhat like '%anh%'or ten_bhat like '%em%'or tieude  like '%yêu%' or tieude  like '%thương%' or tieude  like '%anh%' or tieude  like '%em%'; 
+--
+-- Database: `btth01_cse485`
+--
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `admin`
+--
 
--- tạo view
+CREATE TABLE `admin` (
+  `id` int(10) NOT NULL,
+  `tk` varchar(50) DEFAULT NULL,
+  `mk` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE VIEW vw_Music AS
-SELECT baiviet.ma_bviet,baiviet.noidung, theloai.ten_tloai,tacgia.ten_tgia
-FROM  baiviet, theloai,tacgia;
-SELECT * FROM vw_Music;
+--
+-- Dumping data for table `admin`
+--
 
+INSERT INTO `admin` (`id`, `tk`, `mk`) VALUES
+(1, 'admin', 'admin');
 
-ALTER TABLE theloai
-  ADD SLBaiViet int;
-  
-CREATE TRIGGER tg_CapNhat ON theloai
-FOR {DELETE, INSERT, UPDATE}
-AS 
-  câu_lệnh_sql
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `baiviet`
+--
 
+CREATE TABLE `baiviet` (
+  `ma_bviet` int(10) UNSIGNED NOT NULL,
+  `tieude` varchar(200) NOT NULL,
+  `ten_bhat` varchar(100) NOT NULL,
+  `ma_tloai` int(10) UNSIGNED NOT NULL,
+  `tomtat` text NOT NULL,
+  `noidung` text DEFAULT NULL,
+  `ma_tgia` int(10) UNSIGNED NOT NULL,
+  `ngayviet` datetime NOT NULL DEFAULT current_timestamp(),
+  `hinhanh` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `baiviet`
+--
+
+INSERT INTO `baiviet` (`ma_bviet`, `tieude`, `ten_bhat`, `ma_tloai`, `tomtat`, `noidung`, `ma_tgia`, `ngayviet`, `hinhanh`) VALUES
+(1, 'Lòng mẹ', 'Lòng mẹ', 2, 'Và mẹ ơi đừng khóc nhé! Cả đời này mẹ đã khóc nhiều lắm rồi, hãy cười lên vì con đã trưởng thành! Con sẽ lại về dậy sớm nấu cơm cho mẹ, nấu nước cho mẹ tắm như ngày xưa. “Dù cho vai nắng nhưng lòng thương chẳng nhạt màu, vẫn mơ quay về vui vầy dưới bóng mẹ yêu”', NULL, 1, '2012-07-23 00:00:00', 'images/songs/longme.jpg'),
+(2, 'Cảm ơn em đã rời xa anh', 'Vết mưa', 2, 'Cảm ơn em đã cho anh những tháng ngày hạnh phúc, cho anh biết yêu và được yêu. Em cho anh được nếm trải hương vị ngọt ngào của tình yêu nhưng cũng đầy đau khổ và nước mắt. Những tháng ngày đó có lẽ suốt cuộc đời anh không bao giờ quên', NULL, 3, '2012-02-12 00:00:00', 'images/songs/vetmua.jpg'),
+(3, 'Cuộc đời có mấy ngày mai?', 'Phôi pha', 2, 'Đêm nay, trời quang mây tạnh, trong người nghe hoang vắng và tôi ngồi đây “Ôm lòng đêm, Nhìn vầng trăng mới về” mà ngậm ngùi “Nhớ chân giang hồ. Ôi phù du, từng tuổi xuân đã già”', NULL, 4, '2014-03-13 00:00:00', 'images/songs/phoipha.jpg'),
+(4, 'Quê tôi!', 'Quê hương', 5, 'Quê hương là gì mà chở đầy kí ức nhỏ xinh. Có đám trẻ nô đùa bên nhau dưới gốc ổi nhà bà Năm giữa trưa nắng gắt chỉ để chờ bà đi vắng là hái trộm. Có hai anh em tôi bì bõm lội sình bắt cua đem về nhà cho mẹ nấu canh, nấu cháo… Có ba chị em tôi lục đục tự nấu ăn khi mẹ vắng nhà. Có anh tôi luôn dắt tôi đi cùng đường ngõ xóm chỉ để em được vui. Có cả những trận cãi nhau nảy lửa của ba anh em nữa…', NULL, 5, '2014-02-20 00:00:00', 'images/songs/quehuong.jpg'),
+(5, 'Đất nước', 'Đất nước', 5, 'Đã bao nhiêu lần tôi tự hỏi: liệu trên Thế giới này có nơi nào chiến tranh tang thương mà lại rất đổi anh hùng như nước mình không? Liệu có mảnh đất nào mà mỗi tấc đất hôm nay đã thấm máu xương của những thế hệ đi trước nhiều như nước mình không? Và, liệu có một đất nước nào lại có nhiều bà mẹ đau khổ nhưng cũng hết sức gan góc như đất nước mình không?', NULL, 1, '2010-05-25 00:00:00', 'images/songs/datnuoc.jpg'),
+(6, 'Hard Rock Hallelujah', 'Hard Rock Hallelujah', 7, 'Những linh hồn đang lạc lối, mù quáng mất phương hướng trong cõi trần gian đầy nghiệt ngã hãy nên lắng nghe \"Hard Rock Hallelujah\" để có thể quên tất cả mọi thứ để tìm về đúng bản chất sâu thẳm nhất trong tâm hồn chính mình!', NULL, 6, '2013-09-12 00:00:00', 'images/songs/hardrock.jpg'),
+(7, 'The Unforgiven', 'The Unforgiven', 7, 'Lâu lắm rồi mới nghe lại The Unforgiven II, vì bài này không phải là bài mà tôi thích. Anh bạn tôi lúc trước, đi đâu cũng nghêu ngao bài này ấy, chỉ tại vì hắn đang... thất tình mà lị. Mà sao Metallica có The Unforgiven rồi lại có thêm bài này chi nữa vậy không biết nữa, làm cho tôi cảm thấy hình như hơi bị đúng so với tâm trạng của tôi lúc này.', NULL, 1, '2010-05-25 00:00:00', 'images/songs/TheUnforgiven.jpg'),
+(8, 'Nơi tình yêu bắt đầu', 'Nơi tình yêu bắt đầu', 1, 'Nhiều người sẽ nghĩ làm gì có yêu nhất và làm gì có yêu mãi. Ừ! Chẳng có gì là mãi mãi cả, vì chúng ta không trường tồn vĩnh cửu', NULL, 1, '2014-02-03 00:00:00', 'images/songs/noitinhyeubatdau.jpg'),
+(9, 'Love Me Like There’s No Tomorrow', 'Love Me Like There’s No Tomorrow', 8, 'Nếu ai đã từng yêu Queen, yêu cái chất giọng cao, sắc sảo như một vết cắt thật ngọt ẩn giấu bao cảm xúc mãnh liệt của Freddie chắc không thể không \"điêu đứng\" mỗi khi nghe Love Me Like There’s No Tomorrow.', NULL, 1, '2013-02-26 00:00:00', 'images/songs/loveme.jpg'),
+(10, 'I\'m stronger', 'I\'m stronger', 7, 'Em không phải là người giỏi giấu cảm xúc, nhưng em lại là người giỏi đoán biết cảm xúc của người khác vậy nên đừng cố nói nhớ em, rằng mọi thứ chỉ là do hoàn cảnh. Và cũng đừng dối em rằng anh đã từng yêu em. Em nhắm mắt cũng cảm nhận được mà, thật đấy', NULL, 2, '2013-08-21 00:00:00', 'images/songs/stronger.jpg'),
+(11, 'Ôi Cuộc Sống Mến Thương', 'Ôi Cuộc Sống Mến Thương', 5, 'Có một câu nói như thế này \"Âm nhạc là một cái gì khác lạ mà hầu như tôi muốn nói nó là một phép thần diệu.Vì nó đứng giữa tư tưởng và hiện tượng, tinh thần và vật chất, mọi thứ trung gian mơ hồ thế đó mà không là thế đó giữa các sự vật mà âm nhạc hòa giải\"', NULL, 2, '2011-10-09 00:00:00', 'images/songs/csmt.jpg'),
+(12, 'Cây và gió', 'Cây và gió', 7, 'Em và anh, hai đứa quen nhau thật tình cờ. Lời hát của anh từ bài hát “Cây và gió” đã làm tâm hồn em xao động. Nhưng sự thật phũ phàng rằng em chưa bao giờ nói cho anh biết những suy nghĩ tận sâu trong tim mình. Bởi vì em nhút nhát, em không dám đối mặt với thực tế khắc nghiệt, hay thực ra em không dám đối diện với chính mình.', NULL, 7, '2013-12-05 00:00:00', 'images/songs/cayvagio.jpg'),
+(13, 'Như một cách tạ ơn đời', 'Người thầy', 2, 'Ánh nắng cuối ngày rồi cũng sẽ tắt, dòng sông con đò rồi cũng sẽ rẽ sang một hướng khác. Nhưng việc trồng người luôn cảm thụ với chuyến đò ngang, cứ tần tảo đưa rồi lặng lẽ quay về đưa sang. Con đò năm xưa của Thầy nặng trĩu yêu thương, hy sinh thầm lặng.', NULL, 8, '2014-01-02 00:00:00', 'images/songs/nguoithay.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tacgia`
+--
+
+CREATE TABLE `tacgia` (
+  `ma_tgia` int(10) UNSIGNED NOT NULL,
+  `ten_tgia` varchar(100) NOT NULL,
+  `hinh_tgia` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tacgia`
+--
+
+INSERT INTO `tacgia` (`ma_tgia`, `ten_tgia`, `hinh_tgia`) VALUES
+(1, 'Nhacvietplus', NULL),
+(2, 'Sưu tầm', NULL),
+(3, 'Sandy', NULL),
+(4, 'Lê Trung Ngân', NULL),
+(5, 'Khánh Ngọc', NULL),
+(6, 'Night Stalker', NULL),
+(7, 'Phạm Phương Anh', NULL),
+(8, 'Tâm tình', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `theloai`
+--
+
+CREATE TABLE `theloai` (
+  `ma_tloai` int(10) UNSIGNED NOT NULL,
+  `ten_tloai` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `theloai`
+--
+
+INSERT INTO `theloai` (`ma_tloai`, `ten_tloai`) VALUES
+(1, 'Nhạc trẻ'),
+(2, 'Nhạc trữ tình'),
+(3, 'Nhạc cách mạng'),
+(4, 'Nhạc thiếu nhi'),
+(5, 'Nhạc quê hương'),
+(6, 'POP'),
+(7, 'Rock'),
+(8, 'R&B');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `taikhoan` varchar(50) NOT NULL,
+  `matkhau` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`taikhoan`, `matkhau`) VALUES
+('admin123', 'admin123'),
+('levanthang', '2209');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `baiviet`
+--
+ALTER TABLE `baiviet`
+  ADD PRIMARY KEY (`ma_bviet`),
+  ADD KEY `ma_tgia` (`ma_tgia`),
+  ADD KEY `ma_tloai` (`ma_tloai`);
+
+--
+-- Indexes for table `tacgia`
+--
+ALTER TABLE `tacgia`
+  ADD PRIMARY KEY (`ma_tgia`);
+
+--
+-- Indexes for table `theloai`
+--
+ALTER TABLE `theloai`
+  ADD PRIMARY KEY (`ma_tloai`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `baiviet`
+--
+ALTER TABLE `baiviet`
+  ADD CONSTRAINT `baiviet_ibfk_1` FOREIGN KEY (`ma_tgia`) REFERENCES `tacgia` (`ma_tgia`),
+  ADD CONSTRAINT `baiviet_ibfk_2` FOREIGN KEY (`ma_tloai`) REFERENCES `theloai` (`ma_tloai`);
+COMMIT;
+
+UPDATE baiviet SET hinhanh = 'images/songs/longme.jpg' where ma_bviet = 1;
+UPDATE baiviet SET hinhanh = 'images/songs/vetmua.jpg' where ma_bviet = 2; 
+UPDATE baiviet SET hinhanh = 'images/songs/phoipha.jpg' where ma_bviet = 3;
+UPDATE baiviet SET hinhanh = 'images/songs/quehuong.jpg' where ma_bviet = 4;
+UPDATE baiviet SET hinhanh = 'images/songs/datnuoc.jpg' where ma_bviet = 5;
+UPDATE baiviet SET hinhanh = 'images/songs/hardrock.jpg' where ma_bviet = 6;
+UPDATE baiviet SET hinhanh = 'images/songs/TheUnforgiven.jpg' where ma_bviet = 7;
+UPDATE baiviet SET hinhanh = 'images/songs/noitinhyeubatdau.jpg' where ma_bviet = 8;
+UPDATE baiviet SET hinhanh = 'images/songs/loveme.jpg' where ma_bviet = 9;
+UPDATE baiviet SET hinhanh = 'images/songs/stronger.jpg' where ma_bviet = 10;
+UPDATE baiviet SET hinhanh = 'images/songs/csmt.jpg' where ma_bviet = 11;
+UPDATE baiviet SET hinhanh = 'images/songs/cayvagio.jpg' where ma_bviet = 12;
+UPDATE baiviet SET hinhanh = 'images/songs/nguoithay.jpg' where ma_bviet = 13;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
